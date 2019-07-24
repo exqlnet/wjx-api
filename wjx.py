0a07a86b5a9cae527718064fa32a5744e96a7e91
Exqlnet <exqlnet@gmail.com>
2019-07-24 23:50:50 +0800

    fix: 修复一些会变化的参数导致不能长期运行，优化代码

--- wjx.py
+++ wjx.py
@@ -1,21 +1,21 @@
 # encoding=utf-8
 import requests
 import time
+from vcode import v
 from bs4 import BeautifulSoup
 import urllib
 from logger import log
-from vcode import v
 
 
 class Wjx:
-    """
-    该类对应问卷星的用户
-    能够自动登陆、遇到验证码后调用接口识别验证码登陆
-    """
+
     cookie = ""
     session = requests.session()
     username = ""
     password = ""
+    _VIEWSTATE = ""
+    _EVENTVALIDATION = ""
+    _STATEGENERATOR = ""
 
     def __init__(self, username, password):
 
@@ -107,7 +107,7 @@ class Wjx:
                 log("无验证码，正在登录...")
                 res = self.session.request("POST", "https://www.wjx.cn/login.aspx", data=urllib.parse.urlencode(data), headers=headers)
                 if is_success(res):
-                    log("还是没有验证码更爽😊，登陆成功！")
+                    log("还是没有验证码的时候爽😊，登陆成功！")
                     break
                 log("登陆失败")
             time.sleep(1)
@@ -118,39 +118,61 @@ class Wjx:
             url = "https://www.wjx.cn/wjx/design/designstart.aspx"
 
             querystring = {"activity": activity}
-
-            payload = "__VIEWSTATE=%2FwEPDwUKLTk0MDg2MTE5NA9kFgJmD2QWFgIBD2QWAgIBDxYCHglpbm5lcmh0bWwF%2FAI8YSBocmVmPSdqYXZhc2NyaXB0OiB2b2lkKDApOycgb25jbGljaz0nUERGX2xhdW5jaCgiL3dqeC9tYW5hZ2Uvb25saW5lc3VwcG9ydC5hc3B4IiwgNjIwLCA0MDApOyAnICBjbGFzcz0nYnRuIGJ0bi1kZWZhdWx0IGJ0bi1zbSBidG4tc20tYXBwbHkgJz7nlLPor7fljY%2Fliqk8L2E%2BPGRpdiBjbGFzcz0nZnJlZXByb21vdGV0eHQnIHN0eWxlPSdtYXJnaW4tdG9wOjhweDsnPjxiPuaPkOekuu%2B8mjwvYj7ljYfnuqfliLDkvIHkuJrniYjlj6%2Fkuqvlj5flnKjnur%2Flkqjor6Llj4rnlLXor53mlK%2FmjIHvvIw8YSBocmVmPScgL3JlZ2lzdGVyL3VwZ3JhZGV2aXAuYXNweD91cGdyYWRlUmVhc29uPTE3JyAgY2xhc3M9J3RpdGxlbG5rJz7kuobop6Por6bmg4U8L2E%2BPC9zcGFuPmQCAg8WAh8ABfMBPHNlbGVjdCAgaWQ9J2RkbEFjdGl2aXR5bmV3JyBzdHlsZT0nJz48b3B0aW9uIHZhbHVlPSc0Mjc5NDI5NScgc2VsZWN0ZWQ9J3NlbGVjdGVkJz4yMDE55pqR5pyf5YC854%2Bt4oCc6Zu25oql5ZGK4oCd5oOF5Ya15oql6YCBKElEOjQyNzk0Mjk1KTwvb3B0aW9uPjxvcHRpb24gdmFsdWU9JzQxMjg0NjcxJz7lrabplb%2FlsI%2FmlZnlkZjlupTnn6XlupTkvJrnkIborrrlrabkuaAoSUQ6NDEyODQ2NzEpPC9vcHRpb24%2BPC9zZWxlY3Q%2BZAIDDxYCHgRocmVmBTQvd2p4L2Rlc2lnbi9wcmV2aWV3bW9iaWxlLmFzcHg%2FYWN0aXZpdHk9NDI3OTQyOTUmcz0xZAIEDxYEHwEFLC93angvcHJvbW90ZS9pbnZpdGUuYXNweD9hY3Rpdml0eWlkPTQyNzk0Mjk1HgdWaXNpYmxlaGQCBQ8WBB8BBS8vd2p4L3Byb21vdGUvaW52aXRlc21zLmFzcHg%2FYWN0aXZpdHlpZD00Mjc5NDI5NR8CaGQCBg8WAh8CZ2QCBw8WAh8BBTQvc2FtcGxlL2RlbWFuZC5hc3B4P2FjdGl2aXR5aWQ9NDI3OTQyOTUmbHN0PTEmbnB1Yj0xZAIIDxYEHwEFMC93angvcHJvbW90ZS9qb2luYmFja2xpc3QuYXNweD9hY3Rpdml0eT00Mjc5NDI5NR8CZ2QCCQ8WBB8BBTAvd2p4L3Byb21vdGUvcHJvbW90ZWFwcGx5LmFzcHg%2FYWN0aXZpdHk9NDI3OTQyOTUfAmdkAg0PFgIfAQU0L3dqeC9kZXNpZ24vcHJldmlld21vYmlsZS5hc3B4P2FjdGl2aXR5PTQyNzk0Mjk1JnM9MWQCDw9kFgICAQ9kFgoCAQ8WAh8CZxYEAgEPDxYCHgRUZXh0BWnmraTpl67ljbfmraPlnKjov5DooYzvvIzmgqjlj6%2Fku6U8YSBocmVmPScvcmVwb3J0LzQyNzk0Mjk1LmFzcHgnIGNsYXNzPSd3anhfYWxpbmsnPuafpeeci%2Be7k%2BaenDwvYT7miJbogIVkZAIDDw8WBB8DBRLmmoLlgZzmjqXmlLbnrZTljbceDU9uQ2xpZW50Q2xpY2sFT3JldHVybiBkb0FjdGlvbign54q25oCB6K6%2B5Li64oCc5YGc5q2i4oCd5ZCO5bCG5LiN6IO95aGr5YaZ77yM5piv5ZCm57un57ut77yfJylkZAICDxYCHwJnZAIHDxYCHwEFNy9uZXd3angvZGVzaWduL2VkaXRxdWVzdGlvbm5haXJlLmFzcHg%2FYWN0aXZpdHk9NDI3OTQyOTVkAgkPFgIfAmhkAgwPZBYCAgEPEGRkFgFmZBgBBR5fX0NvbnRyb2xzUmVxdWlyZVBvc3RCYWNrS2V5X18WBAUeY3RsMDIkQ29udGVudFBsYWNlSG9sZGVyMSRyYmxBBR5jdGwwMiRDb250ZW50UGxhY2VIb2xkZXIxJHJibEEFHmN0bDAyJENvbnRlbnRQbGFjZUhvbGRlcjEkcmJsQwUeY3RsMDIkQ29udGVudFBsYWNlSG9sZGVyMSRyYmxD%2FOup%2F2xx4YGkdquUP11Cdd8xPQA%3D&__VIEWSTATEGENERATOR=54ECBFBC&__EVENTVALIDATION=%2FwEdAAfxBLPxM07xxGeIDAjv75L%2BJiB5twIPGrw%2BKcy63oTAP7%2FQCMc5phBLVPhp1UWM0wO4GikaWkkz6p28PrqaxvNKSe3rUU4Mkdn7V7T7lCLTl3gRplDDVJVDDhV2jPDS9TEQmKv%2FwYin78idBCqpeMwvNRzeCqVYNTJuDVQIqkUcqXoL%2BVw%3D&ctl02%24ContentPlaceHolder1%24btnRun=%e6%81%a2%e5%a4%8d%e8%bf%90%e8%a1%8c"
-            res = self.request(url, method="POST", data=payload, params=querystring, content_type="application/x-www-form-urlencoded")
-            log("OK start")
+            payload = {
+                "__VIEWSTATE": self._VIEWSTATE,
+                "__VIEWSTATEGENERATOR": self._STATEGENERATOR,
+                "__EVENTVALIDATION": self._EVENTVALIDATION,
+                "ctl02$ContentPlaceHolder1$btnRun": "恢复运行"
+            }
+            res = self.request(url, method="POST", data=urllib.parse.urlencode(payload),
+                               params=querystring, content_type="application/x-www-form-urlencoded")
+            if self.is_running(activity):
+                log("OK start")
+            else:
+                log("Error start! ", res.status_code)
 
     def stop(self, activity):
         if self.is_running(activity):
             url = "https://www.wjx.cn/wjx/design/designstart.aspx"
 
             querystring = {"activity": activity}
-
-            payload = "__VIEWSTATE=%2FwEPDwUKLTk0MDg2MTE5NA9kFgJmD2QWFgIBD2QWAgIBDxYCHglpbm5lcmh0bWwF%2FAI8YSBocmVmPSdqYXZhc2NyaXB0OiB2b2lkKDApOycgb25jbGljaz0nUERGX2xhdW5jaCgiL3dqeC9tYW5hZ2Uvb25saW5lc3VwcG9ydC5hc3B4IiwgNjIwLCA0MDApOyAnICBjbGFzcz0nYnRuIGJ0bi1kZWZhdWx0IGJ0bi1zbSBidG4tc20tYXBwbHkgJz7nlLPor7fljY%2Fliqk8L2E%2BPGRpdiBjbGFzcz0nZnJlZXByb21vdGV0eHQnIHN0eWxlPSdtYXJnaW4tdG9wOjhweDsnPjxiPuaPkOekuu%2B8mjwvYj7ljYfnuqfliLDkvIHkuJrniYjlj6%2Fkuqvlj5flnKjnur%2Flkqjor6Llj4rnlLXor53mlK%2FmjIHvvIw8YSBocmVmPScgL3JlZ2lzdGVyL3VwZ3JhZGV2aXAuYXNweD91cGdyYWRlUmVhc29uPTE3JyAgY2xhc3M9J3RpdGxlbG5rJz7kuobop6Por6bmg4U8L2E%2BPC9zcGFuPmQCAg8WAh8ABfMBPHNlbGVjdCAgaWQ9J2RkbEFjdGl2aXR5bmV3JyBzdHlsZT0nJz48b3B0aW9uIHZhbHVlPSc0MTI4NDY3MSc%2B5a2m6ZW%2F5bCP5pWZ5ZGY5bqU55%2Bl5bqU5Lya55CG6K665a2m5LmgKElEOjQxMjg0NjcxKTwvb3B0aW9uPjxvcHRpb24gdmFsdWU9JzQyNzk0Mjk1JyBzZWxlY3RlZD0nc2VsZWN0ZWQnPjIwMTnmmpHmnJ%2FlgLznj63igJzpm7bmiqXlkYrigJ3mg4XlhrXmiqXpgIEoSUQ6NDI3OTQyOTUpPC9vcHRpb24%2BPC9zZWxlY3Q%2BZAIDDxYCHgRocmVmBTQvd2p4L2Rlc2lnbi9wcmV2aWV3bW9iaWxlLmFzcHg%2FYWN0aXZpdHk9NDI3OTQyOTUmcz0xZAIEDxYEHwEFLC93angvcHJvbW90ZS9pbnZpdGUuYXNweD9hY3Rpdml0eWlkPTQyNzk0Mjk1HgdWaXNpYmxlaGQCBQ8WBB8BBS8vd2p4L3Byb21vdGUvaW52aXRlc21zLmFzcHg%2FYWN0aXZpdHlpZD00Mjc5NDI5NR8CaGQCBg8WAh8CZ2QCBw8WAh8BBTQvc2FtcGxlL2RlbWFuZC5hc3B4P2FjdGl2aXR5aWQ9NDI3OTQyOTUmbHN0PTEmbnB1Yj0xZAIIDxYEHwEFMC93angvcHJvbW90ZS9qb2luYmFja2xpc3QuYXNweD9hY3Rpdml0eT00Mjc5NDI5NR8CZ2QCCQ8WBB8BBTAvd2p4L3Byb21vdGUvcHJvbW90ZWFwcGx5LmFzcHg%2FYWN0aXZpdHk9NDI3OTQyOTUfAmdkAg0PFgIfAQU0L3dqeC9kZXNpZ24vcHJldmlld21vYmlsZS5hc3B4P2FjdGl2aXR5PTQyNzk0Mjk1JnM9MWQCDw9kFgICAQ9kFgoCAQ8WAh8CZxYEAgEPDxYCHgRUZXh0BWnmraTpl67ljbfmraPlnKjov5DooYzvvIzmgqjlj6%2Fku6U8YSBocmVmPScvcmVwb3J0LzQyNzk0Mjk1LmFzcHgnIGNsYXNzPSd3anhfYWxpbmsnPuafpeeci%2Be7k%2BaenDwvYT7miJbogIVkZAIDDw8WBB8DBRLmmoLlgZzmjqXmlLbnrZTljbceDU9uQ2xpZW50Q2xpY2sFT3JldHVybiBkb0FjdGlvbign54q25oCB6K6%2B5Li64oCc5YGc5q2i4oCd5ZCO5bCG5LiN6IO95aGr5YaZ77yM5piv5ZCm57un57ut77yfJylkZAICDxYCHwJnZAIHDxYCHwEFNy9uZXd3angvZGVzaWduL2VkaXRxdWVzdGlvbm5haXJlLmFzcHg%2FYWN0aXZpdHk9NDI3OTQyOTVkAgkPFgIfAmhkAgwPZBYCAgEPEGRkFgFmZBgBBR5fX0NvbnRyb2xzUmVxdWlyZVBvc3RCYWNrS2V5X18WBAUeY3RsMDIkQ29udGVudFBsYWNlSG9sZGVyMSRyYmxBBR5jdGwwMiRDb250ZW50UGxhY2VIb2xkZXIxJHJibEEFHmN0bDAyJENvbnRlbnRQbGFjZUhvbGRlcjEkcmJsQwUeY3RsMDIkQ29udGVudFBsYWNlSG9sZGVyMSRyYmxDD9eo06cWKTNauU5W6lopaqAO2M4%3D&__VIEWSTATEGENERATOR=54ECBFBC&__EVENTVALIDATION=%2FwEdAAdPlc3R%2Fjgl6MbZMn0KTwg7JiB5twIPGrw%2BKcy63oTAP7%2FQCMc5phBLVPhp1UWM0wO4GikaWkkz6p28PrqaxvNKSe3rUU4Mkdn7V7T7lCLTl3gRplDDVJVDDhV2jPDS9TEQmKv%2FwYin78idBCqpeMwv%2B5PVjLYbAyFkNphjZ7SjmPgvjZc%3D&ctl02%24ContentPlaceHolder1%24btnRun=%E6%9A%82%E5%81%9C%E6%8E%A5%E6%94%B6%E7%AD%94%E5%8D%B7"
-            res = self.request(url, method="POST", data=payload, params=querystring,
+            payload = {
+                "__VIEWSTATE": self._VIEWSTATE,
+                "__VIEWSTATEGENERATOR": self._STATEGENERATOR,
+                "__EVENTVALIDATION": self._EVENTVALIDATION,
+                "ctl02$ContentPlaceHolder1$btnRun": "暂停接收答卷"
+            }
+            res = self.request(url, method="POST", data=urllib.parse.urlencode(payload), params=querystring,
                                content_type="application/x-www-form-urlencoded")
-            log("OK stop")
+            if not self.is_running(activity):
+                log("OK stop")
+            else:
+                log("Error stop!", res.status_code)
 
     def download(self, activity, filename):
-        res = self.request("https://www.wjx.cn/wjx/activitystat/viewstatsummary.aspx?activity={activity}&reportid=-1&dw=1&dt=2".format(activity=activity))
+        res = self.request("https://www.wjx.cn/wjx/activitystat/viewstatsummary.aspx?activity={}&reportid=-1&dw=1&dt=2".format(activity))
         res = requests.get(res.url)
         with open(filename, "wb") as file:
             file.write(res.content)
         log("OK download: ", filename)
 
     def clear(self, activity):
-        payload = "__VIEWSTATE=%2FwEPDwUKLTk0OTgzNTYwOA9kFgICAw9kFgICAQ8QDxYCHgRUZXh0BSvlsIbluo%2Flj7flvZLpm7Yo5q2k6Zeu5Y235b2T5YmN5bqP5Y%2B35Li6MTkpZGRkZBgBBR5fX0NvbnRyb2xzUmVxdWlyZVBvc3RCYWNrS2V5X18WAQUJY2JTZXRaZXJveyz0ClLPCE28yb2%2B%2B%2FsQ0h0fG5g%3D&__VIEWSTATEGENERATOR=2C076B20&__EVENTVALIDATION=%2FwEdAAPvnmVnxrcUrI4Udnygep35wLUC%2FJIggF33CSuxsPN6uzXG%2F58GMRFfvKBtMCp%2FMqIu9Q2TPXECWAXhtfUUlX5WtqxxKw%3D%3D&btnContinue=%E6%B8%85%E7%A9%BA"
-        self.request("https://www.wjx.cn/wjx/activitystat/clearalldata.aspx?activity={}".format(activity), method="post", data=payload, content_type="application/x-www-form-urlencoded")
+        payload = {
+            "__VIEWSTATE": self._VIEWSTATE,
+            "__VIEWSTATEGENERATOR": self._STATEGENERATOR,
+            "__EVENTVALIDATION": self._EVENTVALIDATION,
+            "btnContinue": "清空"
+        }
+        self.request("https://www.wjx.cn/wjx/activitystat/clearalldata.aspx?activity={}".format(activity), method="post",
+                     data=payload, content_type="application/x-www-form-urlencoded")
         log("OK clear data")
 
     def is_running(self, activity):
-        i = 0
-        i += 1
         res = self.request(url="https://www.wjx.cn/wjx/design/designstart.aspx?activity={}".format(activity))
-
+        soup = BeautifulSoup(res.content, "html.parser", from_encoding="utf-8")
+        self._VIEWSTATE = soup.select("#__VIEWSTATE")[0]["value"]
+        self._STATEGENERATOR = soup.select("#__VIEWSTATEGENERATOR")[0]["value"]
+        self._EVENTVALIDATION = soup.select("#__EVENTVALIDATION")[0]["value"]
         if "正在运行" in res.text:
             return True
         else:
