358858b7c9e6f73a3bd6d15904ecf28060910e93
Exqlnet <exqlnet@gmail.com>
2019-07-28 18:06:59 +0800

    fix: 旧版已失效，重写各个接口

--- wjx.py
+++ wjx.py
@@ -22,14 +22,27 @@ class Wjx:
         self.username = username
         self.password = password
 
-    def request(self, url, data=None, method="get", content_type=None, params=None):
+    def request(self, url, data=None, method="GET", content_type=None, params=None, headers=None):
+        if headers is None:
+            headers = {
+                'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+                'accept-encoding': "gzip, deflate, br",
+                'accept-language': "zh-CN,zh;q=0.9,en;q=0.8",
+                'connection': "keep-alive",
+                'host': "www.wjx.cn",
+                'upgrade-insecure-requests': "1",
+                'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36",
+                'cache-control': "no-cache"
+            }
         while True:
             if content_type:
-                res = self.session.request(method, url, data=data, params=params, headers={"Content-Type": content_type})
+                headers["content-type"] = content_type
+                res = self.session.request(method, url, data=data, params=params, headers=headers)
             else:
-                res = self.session.request(method, url, data=data, params=params)
 
-            if "第三方登录" in res.text:
+                res = self.session.request(method, url, data=data, params=params, headers=headers)
+
+            if "第三方登录" in res.text or "未登录" in res.text or "已暂停" in res.text:
                 log("正在尝试登陆...")
                 self.login()
                 time.sleep(1)
@@ -115,14 +128,29 @@ class Wjx:
 
     def start(self, activity):
         if not self.is_running(activity):
-            url = "https://www.wjx.cn/wjx/design/designstart.aspx"
+            param_url = "https://www.wjx.cn/newwjx/manage/myquestionnaires.aspx"
+            # 获取三个参数
+            res = self.request(param_url)
+            soup = BeautifulSoup(res.content, "html.parser", from_encoding="utf-8")
+            __viewstategenerator = soup.select("#__VIEWSTATEGENERATOR")[0]["value"]
+            __viewstate = soup.select("#__VIEWSTATE")[0]["value"]
 
-            querystring = {"activity": activity}
+            url = "https://www.wjx.cn/newwjx/manage/myquestionnaires.aspx"
+
+            querystring = {"randomt": str(int(time.time()) - 60)}
             payload = {
-                "__VIEWSTATE": self._VIEWSTATE,
-                "__VIEWSTATEGENERATOR": self._STATEGENERATOR,
-                "__EVENTVALIDATION": self._EVENTVALIDATION,
-                "ctl02$ContentPlaceHolder1$btnRun": "恢复运行"
+                "__VIEWSTATE": __viewstate,
+                "__VIEWSTATEGENERATOR": __viewstategenerator,
+                'ctl01$ContentPlaceHolder1$hfXingbiao': "",
+                'ctl01$ContentPlaceHolder1$hfFolder': "null",
+                'ctl01$ContentPlaceHolder1$txtName': "请输入问卷名进行搜索...",
+                'ctl01$ContentPlaceHolder1$hidStatus': "-1",
+                'ctl01$ContentPlaceHolder1$sortStyle': "t1",
+                'ctl01$ContentPlaceHolder1$hfCompanyUsers': "null",
+                'ctl01$ContentPlaceHolder1$hfActivity': activity,
+                'ctl01$ContentPlaceHolder1$hfStatus': "-1",
+                'ctl01$ContentPlaceHolder1$hfFolderName': "",
+                'ctl01$ContentPlaceHolder1$btnStatusChange': "发布",
             }
             res = self.request(url, method="POST", data=urllib.parse.urlencode(payload),
                                params=querystring, content_type="application/x-www-form-urlencoded")
@@ -133,17 +161,33 @@ class Wjx:
 
     def stop(self, activity):
         if self.is_running(activity):
-            url = "https://www.wjx.cn/wjx/design/designstart.aspx"
+            param_url = "https://www.wjx.cn/newwjx/manage/myquestionnaires.aspx"
+            # 获取三个参数
+            res = self.request(param_url)
+            soup = BeautifulSoup(res.content, "html.parser", from_encoding="utf-8")
+            __viewstategenerator = soup.select("#__VIEWSTATEGENERATOR")[0]["value"]
+            __viewstate = soup.select("#__VIEWSTATE")[0]["value"]
 
-            querystring = {"activity": activity}
-            payload = {
-                "__VIEWSTATE": self._VIEWSTATE,
-                "__VIEWSTATEGENERATOR": self._STATEGENERATOR,
-                "__EVENTVALIDATION": self._EVENTVALIDATION,
-                "ctl02$ContentPlaceHolder1$btnRun": "暂停接收答卷"
-            }
-            res = self.request(url, method="POST", data=urllib.parse.urlencode(payload), params=querystring,
-                               content_type="application/x-www-form-urlencoded")
+            url = "https://www.wjx.cn/newwjx/manage/myquestionnaires.aspx"
+
+            querystring = {"randomt": str(int(time.time()) - 60)}
+            payload = urllib.parse.urlencode({
+                "__VIEWSTATE": __viewstate,
+                "__VIEWSTATEGENERATOR": __viewstategenerator,
+                'ctl01$ContentPlaceHolder1$hfXingbiao': "",
+                'ctl01$ContentPlaceHolder1$hfFolder': "null",
+                'ctl01$ContentPlaceHolder1$txtName': "请输入问卷名进行搜索...",
+                'ctl01$ContentPlaceHolder1$hidStatus': "-1",
+                'ctl01$ContentPlaceHolder1$sortStyle': "t1",
+                'ctl01$ContentPlaceHolder1$hfCompanyUsers': "null",
+                'ctl01$ContentPlaceHolder1$hfActivity': activity,
+                'ctl01$ContentPlaceHolder1$hfStatus': "2",
+                'ctl01$ContentPlaceHolder1$hfFolderName': "",
+                'ctl01$ContentPlaceHolder1$btnStatusChange': "发布",
+            })
+
+            res = self.request(url, method="POST", data=payload,
+                               params=querystring, content_type="application/x-www-form-urlencoded")
             if not self.is_running(activity):
                 log("OK stop")
             else:
@@ -152,20 +196,37 @@ class Wjx:
     def download(self, activity, filename):
         res = self.request("https://www.wjx.cn/wjx/activitystat/viewstatsummary.aspx?activity={}&reportid=-1&dw=1&dt=2".format(activity))
         res = requests.get(res.url)
+        if "已暂停" in res.text:
+            log("Error download: 没有问卷数据可下载")
+            return
+
         with open(filename, "wb") as file:
             file.write(res.content)
         log("OK download: ", filename)
 
     def clear(self, activity):
-        payload = {
-            "__VIEWSTATE": self._VIEWSTATE,
-            "__VIEWSTATEGENERATOR": self._STATEGENERATOR,
-            "__EVENTVALIDATION": self._EVENTVALIDATION,
+        # 获取三个参数
+        querystring = {"activity": activity}
+        param_url = "https://www.wjx.cn/wjx/activitystat/clearalldata.aspx"
+        res = self.request(param_url, params=querystring)
+        soup = BeautifulSoup(res.content, "html.parser", from_encoding="utf-8")
+        __viewstategenerator = soup.select("#__VIEWSTATEGENERATOR")[0]["value"]
+        __eventvalidation = soup.select("#__EVENTVALIDATION")[0]["value"]
+        __viewstate = soup.select("#__VIEWSTATE")[0]["value"]
+
+        payload = urllib.parse.urlencode({
+            "__VIEWSTATEGENERATOR": __viewstategenerator,
+            "__EVENTVALIDATION": __eventvalidation,
+            "__VIEWSTATE": __viewstate,
             "btnContinue": "清空"
-        }
-        self.request("https://www.wjx.cn/wjx/activitystat/clearalldata.aspx?activity={}".format(activity), method="post",
-                     data=payload, content_type="application/x-www-form-urlencoded")
-        log("OK clear data")
+        })
+        url = "https://www.wjx.cn/wjx/activitystat/clearalldata.aspx"
+        res = self.request(url, method="POST", params=querystring, data=payload, content_type="application/x-www-form-urlencoded")
+
+        if "已成功" in res.text:
+            log("OK clear data")
+        else:
+            log("Error clear data")
 
     def is_running(self, activity):
         res = self.request(url="https://www.wjx.cn/wjx/design/designstart.aspx?activity={}".format(activity))
